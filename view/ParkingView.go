f3a92e8a6a45de308fd3819382db8d385523c9ed
shonsagoro <ramosproque@gmail.com>
2023-10-20 23:09:50 -0600

    add view parking complete xd

--- view/ParkingView.go
+++ view/ParkingView.go
@@ -7,6 +7,7 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
 )
 
 type ParkingView struct {
@@ -22,10 +23,32 @@ func NewParkingView(window fyne.Window) *ParkingView {
 }
 
 func (p *ParkingView) MakeScene() {
+
 	containerParkingView := container.New(layout.NewVBoxLayout())
+	containerParkingOut := container.New(layout.NewHBoxLayout())
+	containerButtons := container.New(layout.NewHBoxLayout())
+
+	restart := widget.NewButton("Restart Simulation", nil)
+	exit := widget.NewButton("Menu Exit", nil)
+
+	containerButtons.Add(restart)
+	containerButtons.Add(exit)
+
+	containerParkingOut.Add(p.MakeWaitStation())
+	containerParkingOut.Add(layout.NewSpacer())
+	containerParkingOut.Add(p.MakeExitStation())
+	containerParkingOut.Add(layout.NewSpacer())
+
+	containerParkingView.Add(containerParkingOut)
+	containerParkingView.Add(layout.NewSpacer())
+	containerParkingView.Add(p.MakeParkingLotEntrance())
+	containerParkingView.Add(layout.NewSpacer())
 	containerParkingView.Add(p.MakeEnterAndExitStation())
 	containerParkingView.Add(layout.NewSpacer())
 	containerParkingView.Add(p.MakeParking())
+	containerParkingView.Add(layout.NewSpacer())
+
+	containerParkingView.Add(container.NewCenter(containerButtons))
 	p.window.SetContent(containerParkingView)
 }
 
@@ -41,21 +64,33 @@ func (p *ParkingView) MakeParking() *fyne.Container {
 }
 
 func (p *ParkingView) MakeWaitStation() *fyne.Container {
-	return nil
+	carsWaitContainer := container.New(layout.NewGridLayout(5))
+	for i := 0; i < 20; i++ {
+		carsWaitContainer.Add(makeSquare())
+	}
+	return carsWaitContainer
+
+}
+func (p *ParkingView) MakeExitStation() *fyne.Container {
+	return container.NewCenter(makeSquare())
 
 }
 
 func (p *ParkingView) MakeEnterAndExitStation() *fyne.Container {
 	parkingContainer := container.New(layout.NewGridLayout(5))
 	parkingContainer.Add(layout.NewSpacer())
-	parkingContainer.Add(makeSquare())
+	enterSquare := makeSquare()
+	// enterSquare.Hide()
+	parkingContainer.Add(enterSquare)
 	parkingContainer.Add(layout.NewSpacer())
-	parkingContainer.Add(makeSquare())
+	exitSquare := makeSquare()
+	// exitSquare.Hide()
+	parkingContainer.Add(exitSquare)
 	parkingContainer.Add(layout.NewSpacer())
 	return container.NewCenter(parkingContainer)
 }
 
-func (p *ParkingView) AddParkingLotEntrance() *fyne.Container {
+func (p *ParkingView) MakeParkingLotEntrance() *fyne.Container {
 	EntraceContainer := container.New(layout.NewGridLayout(3))
 	EntraceContainer.Add(makeBorder())
 	EntraceContainer.Add(layout.NewSpacer())
@@ -76,9 +111,9 @@ func makeSquare() *canvas.Rectangle {
 }
 
 func makeBorder() *canvas.Rectangle {
-	square := canvas.NewRectangle(Gray)
+	square := canvas.NewRectangle(color.RGBA{R: 255, G: 255, B: 255, A: 0})
 	square.SetMinSize(fyne.NewSquareSize(float32(30)))
-	square.StrokeColor = color.RGBA{R: 30, G: 30, B: 30, A: 255}
-	square.StrokeWidth = float32(30)
+	square.StrokeColor = color.RGBA{R: 255, G: 255, B: 255, A: 255}
+	square.StrokeWidth = float32(1)
 	return square
 }
